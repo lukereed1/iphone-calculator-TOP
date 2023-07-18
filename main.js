be58b2029f5f2c10486da626ab171f36d5d74373
Luke Reed <luke.reed@outlook.com.au>
2023-07-18 19:10:38 +1000

    Basic calculations working

--- main.js
+++ main.js
@@ -1,44 +1,65 @@
 let firstOperand;
 let secondOperand;
 let operator;
-let isCleared = true;
+let screenCleared = true;
 
 const digits = document.querySelectorAll(".button");
 const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector("#operate");
+const clear = document.querySelector("#clear");
 const screen = document.querySelector("#screen-numbers");
 
 digits.forEach((digit) =>
-	digit.addEventListener("click", (e) => addDigitsToScreen(e.target.innerHTML))
+	digit.addEventListener("click", (e) => addNumberToScreen(e.target.innerHTML))
 );
 
-function addDigitsToScreen(num) {
-	if (isCleared === true) {
-		isCleared = false;
+operators.forEach((op) => op.addEventListener("click", (e) => setOperator(e.target.innerHTML)));
+
+equals.addEventListener("click", () => operate());
+
+clear.addEventListener("click", () => clearScreen());
+
+function addNumberToScreen(number) {
+	if (screenCleared) {
+		screenCleared = false;
 		screen.innerHTML = " ";
-		screen.innerHTML += num;
-	} else {
-		screen.innerHTML += num;
-	}
+		screen.innerHTML += number;
+	} else screen.innerHTML += number;
 }
 
-operators.forEach((op) => op.addEventListener("click", (e) => getOperator(e.target.innerHTML)));
-
-function getOperator(op) {
-	if (isCleared) return;
-
+function setOperator(op) {
+	if (operator !== null) operate();
+	operator = op;
 	firstOperand = parseFloat(screen.innerHTML);
-	switch (op) {
+	screenCleared = true;
+}
+
+function clearScreen() {
+	screen.innerHTML = "0";
+	screenCleared = true;
+	operator = null;
+	firstOperand = null;
+	secondOperand = null;
+}
+
+function operate() {
+	if (screenCleared || operator === null) return;
+	secondOperand = parseFloat(screen.innerHTML);
+
+	switch (operator) {
 		case "+":
-			operator = "+";
+			screen.innerHTML = firstOperand + secondOperand;
 			break;
 		case "-":
-			operator = "-";
+			screen.innerHTML = firstOperand - secondOperand;
 			break;
 		case "x":
-			operator = "*";
+			screen.innerHTML = firstOperand * secondOperand;
 			break;
 		case "/":
-			operator = "/";
+			screen.innerHTML = firstOperand / secondOperand;
 			break;
 	}
+
+	screenCleared = true;
 }
